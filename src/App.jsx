481b80b09ff6496d520372f01112932442e42582
TaylorTayTyT <taylornguyen904@gmail.com>
2023-03-29 23:53:01 -0400

    stay clean!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      heyo
+    <div className="navigation_bar">
+      <div className='name'>
+      Taylor Nguyen 
+      </div>
+      <div>
+        <button className='nav'>
+          hi
+        </button>
+        <button className='nav'>
+          hi
+        </button>
+        <button className='nav'>
+          hi
+        </button>
+        </div>
     </div>
   )
 }
---
+++ src/MainNavigation.jsx
@@ -0,0 +1,6 @@
+import picOfMe from "./pictures/picOfMe.jpg";
+export default function MainNavigation() {
+    return (
+        <img src = {picOfMe}></img>
+    )
+}
